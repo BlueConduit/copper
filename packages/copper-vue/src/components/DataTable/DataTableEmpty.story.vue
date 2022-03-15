a99e748061dc6aa2ec3f5845b91a257e1785855e
Jackson Hammond <jackson@blueconduit.com>
2022-03-15 15:58:16 -0400

    docs(c-v): add story for custom data table empty state

--- packages/copper-vue/src/components/DataTable/DataTableEmpty.story.vue
+++ packages/copper-vue/src/components/DataTable/DataTableEmpty.story.vue
@@ -1,14 +1,37 @@
 <template>
-  <data-table :data="data" :columns="columns" sortable />
+  <div class="cu-stack-xl">
+    <div class="cu-stack">
+      <p>Base empty table</p>
+      <data-table :data="data" :columns="columns" sortable />
+    </div>
+
+    <div class="cu-stack">
+      <p>
+        Empty table with a custom empty state using the
+        <code>#empty-state</code> slot
+      </p>
+      <data-table :data="data" :columns="columns" sortable>
+        <template #empty-state>
+          <div class="custom-empty-state">
+            <div>Oh geez, Rick, looks like we messed up</div>
+            <Button>Try again!</Button>
+          </div>
+        </template>
+      </data-table>
+    </div>
+  </div>
 </template>
 
 <script>
 import DataTable from "./DataTable.vue";
+import Button from "../Button/Button.vue";
+
 import { ref } from "vue";
 
 export default {
   name: "DataTableEmpty",
   components: {
+    Button,
     DataTable,
   },
   setup() {
@@ -40,3 +63,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.custom-empty-state {
+  margin: 1.5rem auto;
+  padding: 1rem;
+  max-width: 600px;
+  background-color: coral;
+  border-radius: 0.25rem;
+}
+.custom-empty-state div {
+  font-size: 24px;
+}
+</style>
