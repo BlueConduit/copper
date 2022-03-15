a92f8d55b0c004777e496098dcf8fbb0bf77de52
Jackson Hammond <jackson@blueconduit.com>
2022-03-15 15:35:57 -0400

    docs: add a story for empty data tables

---
+++ packages/copper-vue/src/components/DataTable/DataTableEmpty.story.vue
@@ -0,0 +1,42 @@
+<template>
+  <data-table :data="data" :columns="columns" sortable />
+</template>
+
+<script>
+import DataTable from "./DataTable.vue";
+import { ref } from "vue";
+
+export default {
+  name: "DataTableEmpty",
+  components: {
+    DataTable,
+  },
+  setup() {
+    const columns = ref([]);
+    const data = ref([]);
+    columns.value = [
+      {
+        id: "id",
+        label: "ID",
+      },
+      {
+        id: "status",
+        label: "Status",
+      },
+      {
+        id: "address",
+        label: "Address",
+      },
+      {
+        id: "prediction",
+        label: "Prediction",
+      },
+    ];
+
+    return {
+      data,
+      columns,
+    };
+  },
+};
+</script>
